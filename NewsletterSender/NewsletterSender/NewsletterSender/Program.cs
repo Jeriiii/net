0488cc2db7bfb68c8bb5ffab80110e8fdde88536
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-03-16 11:39:20 +0100

    přesunutí a vyčištění projektu

---
+++ NewsletterSender/NewsletterSender/NewsletterSender/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewsletterSender.Dao;
+
+namespace NewsletterSender
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			DB database = new DB();
+			ContactDao contactDao = new ContactDao(database);
+
+			contactDao.NewContact("abc", 1);
+
+			database.Close();
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new HomeWin());
+		}
+	}
+}
